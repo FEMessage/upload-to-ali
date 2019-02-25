14484c3b9113e4d22a9d443ddc5c224d4b970993
evila <ijoec123@gmail.com>
2019-02-25 18:06:24 +0800

    chore: update drag stories

--- stories/drag.vue
+++ stories/drag.vue
@@ -1,30 +1,18 @@
 <template>
   <div class="drag">
     <h2>拖拽上传</h2>
-    <p>拖拽一张图片到下面试试👇 PS: 可在console查看信息</p>
     <upload-to-ali
       v-model="val"
-      accessKeyId="key"
-      accessKeySecret="secret"
-      bucket="bucket"
-      region="region"
-      @loading="loading"
     ></upload-to-ali>
   </div>
 </template>
 
 <script>
-const {log} = console
-
 export default {
   data() {
     return {
-      val: ''
-    }
-  },
-  methods: {
-    loading(name) {
-      log`'上传中:', ${name}`
+      val:
+        '//deepexi-moby.oss-cn-shenzhen.aliyuncs.com/undefinedtooopen_sy_231281461769-1544864776323.jpg'
     }
   }
 }
